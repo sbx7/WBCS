caf5628ad49eb12ee00c1e30036f3792c6499e03
sbx.7 <sbx7@purelymail.com>
2024-09-05 13:51:09 -0700

    Added licence and removed restart explorer message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import ctypes
 path = input("Enter the path of the image:")
 ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 3)
-print("Done! Restart explorer to have the background update.")
+print("Done!")
